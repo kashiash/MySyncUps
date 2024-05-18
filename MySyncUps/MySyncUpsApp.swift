1345135d38e95fd6a5bc259a8d90378c33e918f7
Jacek Kosinski <kashiash@gmail.com>
2024-05-18 16:33:16 +0200

    Initial Commit

---
+++ MySyncUps/MySyncUpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySyncUpsApp.swift
+//  MySyncUps
+//
+//  Created by Jacek Kosinski U on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MySyncUpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
